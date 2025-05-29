ff63a70e53f6c21e07622ee3a3f99b2e13846f7f
vitaliivovchok <71486507+vitalii-vov@users.noreply.github.com>
2025-05-29 12:11:00 -0500

    example updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,10 +14,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _flashlightPlugin = FlashlightFlutter();
   bool _flashAvailable = false;
   bool _torchLevelAvailable = false;
-  double _torchLevel = .0;
-  final _flashlightPlugin = FlashlightFlutter();
+  double _torchLevel = 1;
+  bool _light = false;
 
   @override
   void initState() {
@@ -47,6 +48,20 @@ class _MyAppState extends State<MyApp> {
             Text('Flash available: $_flashAvailable'),
             Text('Torch available: $_torchLevelAvailable'),
             Text('Torch level: $_torchLevel'),
+            Slider(
+              min: 0.1,
+              max: 1,
+              value: _torchLevel,
+              divisions: 9,
+              label: '${_torchLevel * 100}%',
+              onChanged:
+                  _torchLevelAvailable
+                      ? (double value) async {
+                        await _setTorchLevel(value);
+                      }
+                      : null,
+            ),
+
             ElevatedButton(
               onPressed: () async {
                 final torchLevel = await _flashlightPlugin.getTorchLevel();
@@ -57,44 +72,36 @@ class _MyAppState extends State<MyApp> {
               child: Text('Get torch level'),
             ),
 
-            ElevatedButton(
-              onPressed: () async {
-                await _flashlightPlugin.setTorchLevel(0.5);
-                final torchLevel = await _flashlightPlugin.getTorchLevel();
-                setState(() {
-                  _torchLevel = torchLevel;
-                });
+            SizedBox(height: 40),
+            Text(_light ? 'ON' : 'OFF'),
+            Switch(
+              value: _light,
+              onChanged: (bool value) async {
+                await _toggle(value);
               },
-              child: Text('Set torch level: 0.5'),
-            ),
-
-            ElevatedButton(
-              onPressed: () async {
-                await _flashlightPlugin.setTorchLevel(0.7);
-                final torchLevel = await _flashlightPlugin.getTorchLevel();
-                setState(() {
-                  _torchLevel = torchLevel;
-                });
-              },
-              child: Text('Set torch level: 0.7'),
-            ),
-
-            ElevatedButton(
-              onPressed: () async {
-                await _flashlightPlugin.turnOn();
-              },
-              child: Text('ON'),
-            ),
-
-            ElevatedButton(
-              onPressed: () async {
-                await _flashlightPlugin.turnOff();
-              },
-              child: Text('OFF'),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> _toggle(bool value) async {
+    if (value) {
+      await _flashlightPlugin.turnOn();
+    } else {
+      await _flashlightPlugin.turnOff();
+    }
+    setState(() {
+      _light = value;
+    });
+  }
+
+  Future<void> _setTorchLevel(double value) async {
+    await _flashlightPlugin.setTorchLevel(value);
+    setState(() {
+      _torchLevel = value;
+    });
+    _toggle(true);
   }
 }
